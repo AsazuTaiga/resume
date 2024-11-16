5f523d5665daa45e7e41d0ed5937d424a63c6c4e
Asazu Taiga <taiga.asazu@icloud.com>
2024-11-16 12:19:09 +0900

    setup astro and github pages

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://asazutaiga.github.io", // Your GitHub Pages URL
+  base: "/resume", // Your repository name
+});

4553bae8c8bcb27c6ece00518422db3be8b7a0d3
Asazu Taiga <taiga.asazu@icloud.com>
2024-11-16 12:56:27 +0900

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: "https://asazutaiga.github.io", // Your GitHub Pages URL
   base: "/resume", // Your repository name
+  integrations: [mdx()],
 });

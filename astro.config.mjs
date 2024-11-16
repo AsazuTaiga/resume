bf6ff67dbbb877be7bd467cc5bc618a53bcc53e1
Asazu Taiga <taiga.asazu@icloud.com>
2024-11-16 15:10:16 +0900

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, sharpImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://asazutaiga.github.io", // Your GitHub Pages URL
   base: "/resume", // Your repository name
   integrations: [mdx(), tailwind()],
+  image: {
+    service: sharpImageService(),
+  },
 });
